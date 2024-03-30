f16e5a9d12bef2c9acd9fa70b03b93b9e0503d60
Intekhab Imam <111379442+IntekhabImam@users.noreply.github.com>
2024-03-30 17:29:11 +0530

    Header Componetns

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,13 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
-
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
-      
       <BrowserRouter>
-       
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
